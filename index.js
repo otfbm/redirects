d7969ae1bf55a3fb6207984e82c3d716cfdd8bdb
Richard Walker <digitalsadhu@gmail.com>
2020-07-27 22:42:05 +0200

    turn on logging

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fastify = require('fastify');
 
-const server = fastify();
+const server = fastify({ logger: true });
 
 server.get('/*', (request, reply) => {
     const params = request.params["*"];
